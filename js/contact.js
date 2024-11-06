5dc540d9be5bbdcfec2084857891ed8c0c03c425
Emon526 <emonats526@gmail.com>
2024-11-06 20:52:31 +0000

    Deploying to gh-pages from  @ e3bf7c7def76310351a0179df2438e4949429b98 🚀

--- js/contact.js
+++ js/contact.js
@@ -1,77 +1,77 @@
 document.addEventListener("DOMContentLoaded", function () {
-    mapContactDetails();
-    currentDate();
+  mapContactDetails();
+  currentDate();
 });
 
 function mapContactDetails() {
-    let contactInfo = [
-        {
-            "icon": "fa fa-phone",
-            "label": "+92 346 0159889",
-            "link": "https://wa.me/923460159889"
-        },
-        {
-            "icon": "fa fa-envelope",
-            "label": "hamza.6.shakeel@gmail.com",
-            "link": "mailto:hamza.6.shakeel@gmail.com"
-        }
-    ];
+  let contactInfo = [
+    {
+      icon: "fa fa-phone",
+      label: "+880 1849-945526",
+      link: "https://wa.me/8801849945526",
+    },
+    {
+      icon: "fa fa-envelope",
+      label: "emonats526@gmail.com",
+      link: "mailto:emonats526@gmail.com",
+    },
+  ];
 
-    for (var i = 0; i < contactInfo.length; i++) {
-        var c = contactInfo[i];
-        var icon = c.icon;
-        var label = c.label;
-        var link = c.link;
+  for (var i = 0; i < contactInfo.length; i++) {
+    var c = contactInfo[i];
+    var icon = c.icon;
+    var label = c.label;
+    var link = c.link;
 
-        var card = document.createElement("a");
-        card.className = "contact-card";
+    var card = document.createElement("a");
+    card.className = "contact-card";
 
-        card.href = link;
-        card.target = "_blank";
+    card.href = link;
+    card.target = "_blank";
 
-        var iconDiv = document.createElement("i");
-        iconDiv.className = icon;
-        iconDiv.id = "mail-icon";
+    var iconDiv = document.createElement("i");
+    iconDiv.className = icon;
+    iconDiv.id = "mail-icon";
 
-        var labelDiv = document.createElement("span");
-        labelDiv.className = "body2 contact-label";
-        labelDiv.innerHTML = label;
+    var labelDiv = document.createElement("span");
+    labelDiv.className = "body2 contact-label";
+    labelDiv.innerHTML = label;
 
-        var chevDiv = document.createElement("i");
-        chevDiv.id = "chevron-icon";
-        chevDiv.className = "fa fa-chevron-right";
+    var chevDiv = document.createElement("i");
+    chevDiv.id = "chevron-icon";
+    chevDiv.className = "fa fa-chevron-right";
 
-        card.appendChild(iconDiv);
-        card.appendChild(labelDiv);
-        card.appendChild(chevDiv);
+    card.appendChild(iconDiv);
+    card.appendChild(labelDiv);
+    card.appendChild(chevDiv);
 
-        var contactInfoDiv = document.getElementById("contact-info-div");
-        contactInfoDiv.insertBefore(card, contactInfoDiv.firstChild);
-    }
+    var contactInfoDiv = document.getElementById("contact-info-div");
+    contactInfoDiv.insertBefore(card, contactInfoDiv.firstChild);
+  }
 }
 
 function onMeetClick() {
-    window.open("https://calendly.com/mhmzdev/meet", '_blank');
+  window.open("https://calendly.com/developer-asrafulislam/meet", "_blank");
 }
 
 function currentDate() {
-    const current = new Date();
-    let monthName = current.toLocaleDateString('en-US', {
-        month: "long",
-    });
+  const current = new Date();
+  let monthName = current.toLocaleDateString("en-US", {
+    month: "long",
+  });
 
-    let date = current.getDate();
+  let date = current.getDate();
 
-    let dayName = current.toLocaleDateString('en-US', {
-        weekday: "long",
-    });
+  let dayName = current.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
 
-    var monthText = document.getElementById("month");
-    monthText.innerHTML = monthName;
+  var monthText = document.getElementById("month");
+  monthText.innerHTML = monthName;
 
-    var dayText = document.getElementById("date");
-    dayText.innerHTML = date;
+  var dayText = document.getElementById("date");
+  dayText.innerHTML = date;
 
-    var weekDayText = document.getElementById("day");
-    weekDayText.innerText = dayName;
+  var weekDayText = document.getElementById("day");
+  weekDayText.innerText = dayName;
 }
