e3bf7c7def76310351a0179df2438e4949429b98
Emon526 <Emonats526@gmail.com>
2024-11-07 02:51:55 +0600

    info updated

--- src/js/header.js
+++ src/js/header.js
@@ -1,144 +1,143 @@
 document.addEventListener("DOMContentLoaded", function () {
-    mapMetaDataInHeader();
-    mapSocialLinksData();
-    mapStatsData();
+  mapMetaDataInHeader();
+  mapSocialLinksData();
+  mapStatsData();
 });
 
 function mapMetaDataInHeader() {
-    var tagElement = document.getElementById("tag-label");
-    tagElement.innerText = 'Full Stack Flutter Expert';
+  var tagElement = document.getElementById("tag-label");
+  tagElement.innerText = "Full Stack Flutter Expert";
 
-    let firstName = 'Muhammad';
-    let lastName = 'Hamza';
+  let firstName = "Asraful";
+  let lastName = "Islam";
 
-    var firstNameElement = document.getElementById("first-name");
-    firstNameElement.innerHTML = firstName;
+  var firstNameElement = document.getElementById("first-name");
+  firstNameElement.innerHTML = firstName;
 
-    var lastNameElement = document.getElementById("last-name");
-    lastNameElement.innerHTML = lastName;
-
+  var lastNameElement = document.getElementById("last-name");
+  lastNameElement.innerHTML = lastName;
 }
 
 function mapSocialLinksData() {
-    let socials = [
-        {
-            "icon": "fa fa-linkedin-square",
-            "value": "https://linkedin.com/in/mhmzdev"
-        },
-        {
-            "icon": "fa fa-github",
-            "value": "https://github.com/mhmzdev"
-        },
-        {
-            "icon": "fa fa-medium",
-            "value": "https://mhmzdev.medium.com"
-        },
-        {
-            "icon": "fa fa-facebook-square",
-            "value": "https://facebook.com/mhmzdev"
-        },
-        {
-            "icon": "fa fa-instagram",
-            "value": "https://instagram.com/mhmzdev"
-        },
-        {
-            "icon": "fa fa-twitter",
-            "value": "https://twitter.com/mhmzdev"
-        }
-    ];
+  let socials = [
+    {
+      icon: "fa fa-linkedin-square",
+      value: "https://linkedin.com/in/asraful-islam-emon",
+    },
+    {
+      icon: "fa fa-github",
+      value: "https://github.com/emon526",
+    },
+    // {
+    //   icon: "fa fa-medium",
+    //   value: "https://mhmzdev.medium.com",
+    // },
+    {
+      icon: "fa fa-facebook-square",
+      value: "https://facebook.com/emon.asrafulislam",
+    },
+    {
+      icon: "fa fa-instagram",
+      value: "https://instagram.com/asraful_islam_emon",
+    },
+    {
+      icon: "fa fa-twitter",
+      value: "https://twitter.com/emon526",
+    },
+  ];
 
-    // icons in top section
-    for (var i = 0; i < socials.length; i++) {
-        var anchor = document.createElement("a");
-        anchor.className = "social-icon-wrapper";
+  // icons in top section
+  for (var i = 0; i < socials.length; i++) {
+    var anchor = document.createElement("a");
+    anchor.className = "social-icon-wrapper";
 
-        var icon = document.createElement("i");
-        icon.className = "social-icon " + socials[i]["icon"];
+    var icon = document.createElement("i");
+    icon.className = "social-icon " + socials[i]["icon"];
 
-        var link = socials[i]["value"];
-        anchor.href = link;
-        anchor.target = "_blank";
+    var link = socials[i]["value"];
+    anchor.href = link;
+    anchor.target = "_blank";
 
-        anchor.appendChild(icon);
+    anchor.appendChild(icon);
 
-        var socialHandles = document.getElementById("social-handles");
-        socialHandles.appendChild(anchor);
-    }
+    var socialHandles = document.getElementById("social-handles");
+    socialHandles.appendChild(anchor);
+  }
 
-    // icons in contact section
-    for (var i = 0; i < socials.length; i++) {
-        var anchor = document.createElement("a");
-        anchor.className = "social-icon-wrapper";
-        anchor.style.padding = "3%";
+  // icons in contact section
+  for (var i = 0; i < socials.length; i++) {
+    var anchor = document.createElement("a");
+    anchor.className = "social-icon-wrapper";
+    anchor.style.padding = "3%";
 
-        var icon = document.createElement("i");
-        icon.className = "social-icon " + socials[i]["icon"];
+    var icon = document.createElement("i");
+    icon.className = "social-icon " + socials[i]["icon"];
 
-        var link = socials[i]["value"];
-        anchor.href = link;
-        anchor.target = "_blank";
+    var link = socials[i]["value"];
+    anchor.href = link;
+    anchor.target = "_blank";
 
-        anchor.appendChild(icon);
+    anchor.appendChild(icon);
 
-        var socialHandlsContact = document.getElementById("social-contact-icons");
-        var extraDiv = document.createElement("div");
+    var socialHandlsContact = document.getElementById("social-contact-icons");
+    var extraDiv = document.createElement("div");
 
-        socialHandlsContact.appendChild(extraDiv);
-        socialHandlsContact.appendChild(anchor);
-        socialHandlsContact.appendChild(extraDiv);
-    }
+    socialHandlsContact.appendChild(extraDiv);
+    socialHandlsContact.appendChild(anchor);
+    socialHandlsContact.appendChild(extraDiv);
+  }
 }
 
 function mapStatsData() {
-    let stats = [
-        {
-            "value": "~5",
-            "text-1": "Years",
-            "text-2": "Experience"
-        },
-        {
-            "value": "40+",
-            "text-1": "Projects Completed",
-            "text-2": "in 10+ Countries"
-        },
-        {
-            "value": "~120k",
-            "text-1": "Content",
-            "text-2": "Reach & Views"
-        }
-    ];
+  let stats = [
+    {
+      value: "~2",
+      "text-1": "Years",
+      "text-2": "Experience",
+    },
+    {
+      value: "15+",
+      "text-1": "Projects Completed",
+      "text-2": "in 8+ Countries",
+    },
+    {
+      value: "~12k",
+      "text-1": "Content",
+      "text-2": "Reach & Views",
+    },
+  ];
 
-    for (var i = 0; i < stats.length; i++) {
-        var stat = stats[i];
-        var value = stat["value"];
-        var txt1 = stat["text-1"];
-        var txt2 = stat["text-2"];
+  for (var i = 0; i < stats.length; i++) {
+    var stat = stats[i];
+    var value = stat["value"];
+    var txt1 = stat["text-1"];
+    var txt2 = stat["text-2"];
 
-        var div = document.createElement("div");
-        div.className = "stats stats-data";
+    var div = document.createElement("div");
+    div.className = "stats stats-data";
 
-        var valueText = document.createElement("h1");
-        valueText.className = "value";
-        valueText.textContent = value;
+    var valueText = document.createElement("h1");
+    valueText.className = "value";
+    valueText.textContent = value;
 
-        var textColumn = document.createElement("div");
-        textColumn.className = "stats-label-column";
+    var textColumn = document.createElement("div");
+    textColumn.className = "stats-label-column";
 
-        var text1 = document.createElement("div");
-        text1.className = "label stats-label";
-        text1.innerHTML = txt1;
+    var text1 = document.createElement("div");
+    text1.className = "label stats-label";
+    text1.innerHTML = txt1;
 
-        var text2 = document.createElement("div");
-        text2.className = "label stats-label";
-        text2.innerHTML = txt2;
+    var text2 = document.createElement("div");
+    text2.className = "label stats-label";
+    text2.innerHTML = txt2;
 
-        textColumn.appendChild(text1);
-        textColumn.appendChild(text2);
+    textColumn.appendChild(text1);
+    textColumn.appendChild(text2);
 
-        div.appendChild(valueText);
-        div.appendChild(textColumn);
+    div.appendChild(valueText);
+    div.appendChild(textColumn);
 
-        var overaAllStatsDiv = document.getElementById("overall-stats");
-        overaAllStatsDiv.append(div);
-    }
+    var overaAllStatsDiv = document.getElementById("overall-stats");
+    overaAllStatsDiv.append(div);
+  }
 }
